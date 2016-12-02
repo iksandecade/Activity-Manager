070f9df52b46e08cb81bd2874fa87b628fd89d8c
iksandecade <iksandecade@gmail.com>
2016-12-02 14:14:28 +0700

    activity manager for lollipop and above

--- app/src/main/java/project/iksandecade/activitymanager/MainActivity.java
+++ app/src/main/java/project/iksandecade/activitymanager/MainActivity.java
@@ -1,21 +1,53 @@
 package project.iksandecade.activitymanager;
 
-import android.app.ActivityManager;
+import android.annotation.TargetApi;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(getBaseContext(), Services.class);
-        startService(i);
+        context = this;
+        if (doIHavePermission()) {
+            Toast.makeText(context, "Congrats", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
+
     }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public boolean doIHavePermission() {
+
+        Calendar beginCal = Calendar.getInstance();
+        beginCal.set(Calendar.DATE, 1);
+        beginCal.set(Calendar.MONTH, 0);
+        beginCal.set(Calendar.YEAR, 2012);
+
+        Calendar endCal = Calendar.getInstance();
+        endCal.set(Calendar.DATE, 1);
+        endCal.set(Calendar.MONTH, 0);
+        endCal.set(Calendar.YEAR, 2017);
+        final UsageStatsManager usageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+        final List<UsageStats> queryUsageStats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, beginCal.getTimeInMillis(), endCal.getTimeInMillis());
+
+        return !queryUsageStats.isEmpty();
+    }
+
+
 }
--- app/src/main/java/project/iksandecade/activitymanager/Services.java
+++ app/src/main/java/project/iksandecade/activitymanager/Services.java
@@ -2,17 +2,23 @@ package project.iksandecade.activitymanager;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.SortedMap;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.TreeMap;
 
 /**
  * Created by
@@ -25,6 +31,7 @@ import java.util.TimerTask;
 
 public class Services extends Service {
     Timer timer = new Timer();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -33,8 +40,8 @@ public class Services extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-//       callAsynchronousTask();
-//        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+//        callAsynchronousTask();
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
         List<ProcessManager.Process> processes = ProcessManager.getRunningApps();
         for (int i = 0; i < processes.size(); i++) {
             Log.d("Executed app", "Application executed : " + processes.get(i).name);
@@ -50,6 +57,7 @@ public class Services extends Service {
             @Override
             public void run() {
                 handler.post(new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                     public void run() {
                         final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
                         final List<ActivityManager.RunningAppProcessInfo> recentTasks = activityManager.getRunningAppProcesses();
@@ -64,4 +72,33 @@ public class Services extends Service {
         timer.schedule(doAsynchronousTask, 0, 2000); //execute in every 50000 ms
 
     }
+
+    private String getProcessName() {
+        String foregroundProcess = "";
+        ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(ACTIVITY_SERVICE);
+        // Process running
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            UsageStatsManager mUsageStatsManager = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
+            long time = System.currentTimeMillis();
+            // We get usage stats for the last 10 seconds
+            List<UsageStats> stats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 10, time);
+            // Sort the stats by the last time used
+            if (stats != null) {
+                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
+                for (UsageStats usageStats : stats) {
+                    mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+                }
+                if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                    String topPackageName = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                    foregroundProcess = topPackageName;
+                }
+            }
+        } else {
+            @SuppressWarnings("deprecation") ActivityManager.RunningTaskInfo foregroundTaskInfo = activityManager.getRunningTasks(1).get(0);
+            foregroundProcess = foregroundTaskInfo.topActivity.getPackageName();
+
+        }
+        return foregroundProcess;
+    }
+
 }
